b015d103c4166ba8b53e337d5a6818ce6411ff13
ryz2593 <805445415@qq.com>
2019-05-30 14:01:02 +0800

    update

--- src/main/java/com/ryz2593/leetcode/RemoveElement.java
+++ src/main/java/com/ryz2593/leetcode/RemoveElement.java
@@ -1,6 +1,7 @@
 package com.ryz2593.leetcode;
 
 /**
+ * 27.
  * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
  * <p>
  * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
