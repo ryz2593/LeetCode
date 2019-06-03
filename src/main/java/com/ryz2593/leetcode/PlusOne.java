468fa58b9ab49ee2ae50a54170b827f91c477b5d
ryz2593 <805445415@qq.com>
2019-06-03 09:38:31 +0800

    add & update

--- src/main/java/com/ryz2593/leetcode/PlusOne.java
+++ src/main/java/com/ryz2593/leetcode/PlusOne.java
@@ -29,7 +29,7 @@ import java.util.List;
  * @desc
  */
 public class PlusOne {
-    
+
     public static int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] != 9) {
